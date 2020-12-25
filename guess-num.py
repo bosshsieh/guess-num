983fac126563a2b26e618fe1817ebdd2b5296a84
boss hsieh <bosshsieh717@gmail.com>
2020-12-25 21:55:48 +0800

    Final verdion

--- guess-num.py
+++ guess-num.py
@@ -1,11 +1,15 @@
-#產生一個隨機整數1~100(不要印出來)
+#讓使用者自訂隨機產出數字範圍
 #讓使用者重複猜數字
 #猜對的話 印出 "終於猜對了"
 #猜錯的話 要告訴他 比答案大/小
+#計算猜的次數
 
 import random
-
-r = random.randint(1,100)
+start = input('請決定隨機數字範圍開始值:')
+end = input('請決定隨機數字範圍開始值:')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0
 while True:
 	# print(r)
